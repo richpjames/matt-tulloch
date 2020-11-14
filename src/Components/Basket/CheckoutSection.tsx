8a30e37e979a154f107851ef6bed33d0dbf01058
richpjames <rich@Richards-MacBook-Pro.local>
2020-11-14 18:52:45 +0000

    create checkout function

--- src/Components/Basket/CheckoutSection.tsx
+++ src/Components/Basket/CheckoutSection.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { loadStripe } from "@stripe/stripe-js";
 import styled from "styled-components";
 
 import { twoDecimalPlaces } from "../../utils";
 
+import { CartContext } from "./CartProvider";
 import { Shipping } from "./Shipping";
 import { CTAButton } from "../Common";
 import { BasketTotal } from "./BasketTotal";
+import { stripePublishableKey } from "../../constants";
+
+const stripePromise = loadStripe(stripePublishableKey || "");
 
 const CheckoutSectionWrap = styled.section`
   padding-top: 2rem;
@@ -17,18 +22,46 @@ const CheckoutSectionWrap = styled.section`
 interface Props {
   setShipping: (index: number) => void;
   shipping: Shipping;
-  onCheckoutClicked: (click: React.MouseEvent) => void;
   hasItems: boolean;
   total: string;
 }
 
 export const CheckoutSection: React.FC<Props> = ({
   hasItems,
-  onCheckoutClicked,
   setShipping,
   shipping,
-  total,
 }) => {
+  const { cart, total } = useContext<BasketContext>(CartContext);
+  const onCheckoutClicked = () => {
+    debugger;
+    const lineItems = cart.map(([sku, quantity]) => ({
+      price: sku.id,
+      quantity,
+    }));
+    fetch("/.netlify/functions/orderCreate", {
+      method: "POST",
+      body: JSON.stringify(lineItems),
+    })
+      .then(async (response) => {
+        const { id } = await response.json();
+        localStorage.setItem("cart", "{}");
+        const stripe = await stripePromise;
+        if (stripe) {
+          const stripeResponse = await stripe.redirectToCheckout({
+            sessionId: id,
+          });
+
+          if (stripeResponse.error) {
+            alert(stripeResponse.error.message);
+          }
+          // If `redirectToCheckout` fails due to a browser or network
+          // error, display the localized error message to your customer
+          // using `error.message`.
+        }
+      })
+      .catch((err) => alert(err.message));
+  };
+
   return (
     <CheckoutSectionWrap>
       <Shipping shipping={shipping} setShipping={setShipping} />
