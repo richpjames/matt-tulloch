e93a4e6280fb54221b8063c9827c789cff908c91
richpjames <rich@Richards-MacBook-Pro.local>
2020-11-15 21:23:41 +0000

    add products provider

---
+++ src/Components/ProductsProvider.jsx
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
+
+export const ProductsContext = React.createContext();
+
+/**
+ * Wrapper to give Provider access to Sku nodes from Gatsby's GraphQL store.
+ */
+const ProductsProvider = ({ children }) => {
+  const data = useStaticQuery(skusQuery);
+  return <Provider data={data}>{children}</Provider>;
+};
+
+ProductsProvider.propTypes = {
+  children: PropTypes.any.isRequired,
+};
+
+/**
+ * Shares product & sku data through Context.
+ * Products are first loaded from Gatsby's GraphQL store and then updated with
+ * current information from Stripe.
+ */
+const Provider = ({ data, children }) => {
+  // Load product data from Gatsby store
+  const [initialProducts, skus] = processGatsbyData(data);
+  const [products, setProducts] = useState(initialProducts);
+
+  return (
+    <ProductsContext.Provider
+      value={{
+        products,
+        skus,
+        listProducts: (sortFn) => {
+          const fn = sortFn || ((a, b) => b.price - a.price);
+          return Object.values(products).sort(fn);
+        },
+      }}
+    >
+      {children}
+    </ProductsContext.Provider>
+  );
+};
+
+Provider.propTypes = {
+  data: PropTypes.object.isRequired,
+  children: PropTypes.any.isRequired,
+};
+
+/** Normalize structure of data sourced from Gatsby's GraphQL store */
+const processGatsbyData = (data) => {
+  const { allImageSharp, allProductsJson } = data;
+
+  const products = [];
+  const skus = {};
+  allProductsJson.nodes.forEach((productData) => {
+    const {
+      prodPriceId,
+      devPriceId,
+      price,
+      title,
+      inventory,
+      dimensions,
+      slug,
+    } = productData;
+    console.log(title, "title");
+    const id = process.env.NODE_ENV === "production" ? prodPriceId : devPriceId;
+    const imageQuery = new RegExp(String.raw`${slug}`);
+    const image = allImageSharp.nodes.find((image) =>
+      imageQuery.test(image.fluid.src)
+    );
+    const product = {
+      price: +price,
+      title,
+      inventory,
+      dimensions,
+      slug,
+      image,
+    };
+    productData.skus = allProductsJson.nodes.map((product) => {
+      skus[id] = product;
+      return product;
+    });
+    products.push(product);
+    products[id] = product;
+  });
+  return [products, skus];
+};
+
+const skusQuery = graphql`
+  {
+    allProductsJson {
+      nodes {
+        price
+        title
+        prodPriceId
+        devPriceId
+        inventory
+        slug
+        dimensions
+      }
+    }
+    allImageSharp {
+      nodes {
+        fluid {
+          ...GatsbyImageSharpFluid_withWebp_noBase64
+        }
+      }
+    }
+  }
+`;
+
+export default ProductsProvider;
