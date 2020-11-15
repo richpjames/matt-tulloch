306733ea9e49d61bd0ef76d9341e2123f7d1e23d
richpjames <rich@Richards-MacBook-Pro.local>
2020-11-15 21:20:31 +0000

    move provider to layout

--- src/pages/basket.tsx
+++ src/pages/basket.tsx
@@ -2,17 +2,14 @@ import React from "react";
 
 import { SmallLogo } from "../Components/Common/";
 import { Layout } from "../Components/layout";
-import CartProvider from "../Components/Basket/CartProvider";
 import Basket from "../Components/Basket/Basket";
 
 const BasketContainer = () => {
   return (
-    <CartProvider>
-      <Layout>
-        <SmallLogo />
-        <Basket />
-      </Layout>
-    </CartProvider>
+    <Layout>
+      <SmallLogo />
+      <Basket />
+    </Layout>
   );
 };
 export default BasketContainer;
