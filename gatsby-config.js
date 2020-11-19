db8988a381dad20616ebae146425482a8ca7b04c
richpjames <rich@Richards-MacBook-Pro.local>
2020-11-19 21:26:11 +0000

    add typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     `gatsby-transformer-json`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-
+    `gatsby-plugin-typescript`,
     {
       resolve: "gatsby-source-filesystem",
       options: {
