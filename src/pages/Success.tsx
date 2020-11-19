b874952ecf2c748455bbe36d105c75f52ad5f67b
richpjames <rich@Richards-MacBook-Pro.local>
2020-11-19 08:53:08 +0000

    remove title for now

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -1,19 +1,14 @@
 import React from "react";
 import { RouteComponentProps } from "@reach/router";
 
-import {
-  InfoSection,
-  PageWrapper,
-  PageTitle,
-  Text,
-} from "../Components/Common";
+import { InfoSection, Text } from "../Components/Common";
 import { Layout } from "../Components/layout";
 
 const Success: React.FC<RouteComponentProps> = () => {
   return (
     <Layout>
       <InfoSection>
-        <PageTitle title="Success" />
+        {/* <PageTitle title="Success" /> */}
         <Text
           text={`<p>Everything went through ok.<br /> Thanks for your order. <br /> You will receive an email with details. <br /> For enquiries contact@monitorbooks.co.uk</p>`}
         />
