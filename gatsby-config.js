1adeab797fd3bc33688185e0fb1e65bc7801701a
richpjames <rich@Richards-MacBook-Pro.local>
2020-11-21 13:21:06 +0000

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "./src/favicon.png",
+        icon: "./src/favicon.svg",
       },
     },
     {
