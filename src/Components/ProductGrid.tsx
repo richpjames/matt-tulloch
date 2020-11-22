15ebfa40644ff8de52db3919d59c502afb621bba
richpjames <rich@Richards-MacBook-Pro.local>
2020-11-22 20:38:33 +0000

    loose border

--- src/Components/ProductGrid.tsx
+++ src/Components/ProductGrid.tsx
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import styled from "styled-components/macro";
 import { Link } from "gatsby";
-import { text } from "../constants";
 import { ProductsContext } from "./ProductsProvider";
 
 import Image from "gatsby-image";
@@ -9,7 +8,7 @@ const GridWrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   grid-gap: 2rem;
-  padding-top: 2rem;
+  padding-top: 5rem;
   @media only screen and (max-width: 600px) {
     grid-template-columns: 1fr;
   }
@@ -18,7 +17,6 @@ const GridWrapper = styled.div`
 const GridItem = styled(Image)<{ fluid: any }>`
   height: 15rem;
   width: 15rem;
-  border: 0.5rem solid ${text};
 `;
 
 export const ProductGrid = () => {
