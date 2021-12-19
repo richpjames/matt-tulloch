1782cd50ebe97cc787638516331906be45cf595d
richpjames <rich@Richards-MacBook-Pro.local>
2021-12-19 20:51:07 +0000

    add log

--- src/cms/cmj.js
+++ src/cms/cmj.js
@@ -2,4 +2,6 @@ import CMS from "netlify-cms-app";
 
 import ProductPagePreview from "./preview-templates/ProductPagePreview";
 
+console.log("im being red");
+
 CMS.registerPreviewTemplate("shop_items", ProductPagePreview);
